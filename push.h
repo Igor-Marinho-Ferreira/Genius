d89a3f9e2a797de30a5a650cbe4abff12512d15e
Igor Marinho <86384484+Igor-Marinho-Ferreira@users.noreply.github.com>
2022-08-16 16:56:45 -0300

    Att - IGOR

---
+++ push.h
@@ -0,0 +1,25 @@
+#ifndef PUSH_H_INCLUDED
+#define PUSH_H_INCLUDED
+
+#include<stdio.h>
+#include<stdlib.h>
+
+struct no{
+	char elem;
+	struct no *prox;
+};
+
+void push(int valor, struct no **t){
+	struct no *novo;
+	novo = (struct no *) malloc (sizeof(struct no));
+	novo->elem = valor;
+	if( (*t) == NULL ){
+		(*t) = novo;
+		(*t)->prox = NULL;
+	} else {
+		novo->prox = (*t);
+		(*t)=novo;
+	}
+}
+
+#endif
